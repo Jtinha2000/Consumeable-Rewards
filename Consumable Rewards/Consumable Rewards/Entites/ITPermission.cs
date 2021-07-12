11245792fcb2c9d49206c7e2b55b9d3979c51fc7
CommunistDoggu <77682013+Jtinha2000@users.noreply.github.com>
2021-07-12 18:47:53 -0300

    Plugin Base

---
+++ Consumable Rewards/Consumable Rewards/Entites/ITPermission.cs
@@ -0,0 +1,31 @@
+﻿namespace GroupItemPermissions
+{
+    sealed public class ITPermission
+    {
+        public string Name { get; set; }
+        public ushort Id { get; set; }
+        public string ConsumeMessage { get; set; }
+        public bool HasConsumeMessage { get; set; }
+        public bool RewardHasGroup { get; set; }
+        public string Permission { get; set; }
+        public bool RewardHasExperience { get; set; }
+        public int ExperienceAmount { get; set; }
+
+        public ITPermission()
+        {
+
+        }
+
+        public ITPermission(string name, ushort id, string consumeMessage, bool hasConsumeMessage, bool rewardHasGroup, string permission, bool rewardHasExperience, int esperienceAmount)
+        {
+            Name = name;
+            Id = id;
+            ConsumeMessage = consumeMessage;
+            HasConsumeMessage = hasConsumeMessage;
+            RewardHasGroup = rewardHasGroup;
+            Permission = permission;
+            RewardHasExperience = rewardHasExperience;
+            ExperienceAmount = esperienceAmount;
+        }
+    }
+}
